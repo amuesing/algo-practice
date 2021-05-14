e62aa8b14ff781257ffaa61614a51c243903a4f6
“Aaron <muesing.music@gmail.com>
2021-05-13 21:29:18 -0400

    understanding the problem

---
+++ Big O Notation/19. Understand The Problem.js
@@ -0,0 +1,27 @@
+// Write a function which takes two numbers and returns their sum.
+
+// 1. Can I restate the problem in my own words?
+"implement addition"
+// 2. What are the inputs that go into the problem?\
+    // -ints?
+    // -floats?
+    // -what about string for large numbers?
+
+// 3. What are the outputs that should come from the solution to the problem?
+    // -int? float? string?
+// 4. Can the outputs be determined from the inputs? In other words, ddo I have enough information to solve the problem
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+
+
+// Start with Simple Examples
+// Progress to More Complex Examples
+// Explore Examples with Empty Inputs
+// Explore Examples with Invalid Inputs
+
+
+// Write a function which takes in a string and returns counts of each character in the string
+
+
+charCount("aaaa"); // {a:4}
+charCount("hello"); // {h:1, e:1, l:2, o:1}
