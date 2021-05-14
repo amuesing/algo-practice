5c6ecac2944769085003254dd05e8b80099d37d2
“Aaron <muesing.music@gmail.com>
2021-05-14 13:56:00 -0400

    22.solve or simplify

--- algo-basics/understanding-the-problem.js
+++ algo-basics/understanding-the-problem.js
@@ -1,7 +1,7 @@
 // Write a function which takes two numbers and returns their sum.
 
 // 1. Can I restate the problem in my own words?
-"implement addition"
+// "implement addition"
 // 2. What are the inputs that go into the problem?\
     // -ints?
     // -floats?
@@ -23,5 +23,27 @@
 // Write a function which takes in a string and returns counts of each character in the string
 
 
-charCount("aaaa"); // {a:4}
-charCount("hello"); // {h:1, e:1, l:2, o:1}
+// charCount("aaaa"); // {a:4}
+// charCount("hello"); // {h:1, e:1, l:2, o:1}
+
+function charCount(str) {
+    // make object to return at end
+    var result = {};
+    // loop over string, for each character..
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i].toLowerCase();
+        // if the char is a number/letter AND is a key in object, add one to count
+        if(result[char] > 0) {
+            result[char]++;   
+        }
+        // if the char is a number/letter AND not object, add it to object and set value to 1
+        else {
+            result[char] = 1;
+        }
+    }
+    // if character is something else (space, period, etc.) don't do anything
+    // return object at end
+    return result
+}
+
+debugger
