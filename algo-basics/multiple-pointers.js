3a8bab1d8f0f8e7a52653ee97533d2fc518ae8ca
“Aaron <muesing.music@gmail.com>
2021-05-19 17:34:58 -0400

    countUniqueValues

--- algo-basics/multiple-pointers.js
+++ algo-basics/multiple-pointers.js
@@ -50,4 +50,24 @@ function sumZero(arr) {
 // Time Complexity - O(N)
 // Space Complexity - O(1)
 
-console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))
+
+//** countUniqueValues */
+
+// Implement a function called countUniqueValues,
+// which accepts a sorted array, and counts the
+// unique values in the array. There can be negative
+// numbers in the array, but it will always be sorted.
+const countUniqueValues = (arr) => {
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i + 1;
+}
+
+console.log(countUniqueValues([1,1,1,2,2,3,4,5,5,5,6,7]))
