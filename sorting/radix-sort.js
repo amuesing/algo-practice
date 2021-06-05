57a9c205b66885c5c9f761170f74add2ecd099bf
“Aaron <muesing.music@gmail.com>
2021-06-05 11:45:04 -0400

    radix sort implementation

--- sorting/radix-sort.js
+++ sorting/radix-sort.js
@@ -35,5 +35,34 @@ function mostDigits(nums) {
     for (let i = 0; i < nums.length; i++) {
         maxDigits = Math.max(maxDigits, digitCount(nums[i]));
     }
-    return maxDigits;
+    return maxDigits; 
 }
+
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+    for(let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k);
+            digitBuckets[digit].push(nums[i]);
+        }
+        nums = [].concat(...digitBuckets);
+    }
+    return nums;
+}
+
+//** Pseudocode **//
+
+// Define a function that accepts list of numbers
+
+// Figure out how many digits the largest number has
+
+// Loop from k=0 up to this largest number of digits
+
+// For each iteration of the loop:
+    // Create buckets for each digit (0 to 9)
+    // place each number in the corresponding bucket based on its *k*th digit
+
+// Replace our existing array with values in our buckets, starting with 0 and going up to 9
+
+// return list at the end
